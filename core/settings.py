15f95b96e522c964e2798862dfe4e54467c34126
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-12-03 15:17:46 +0330

    feat: update core module and add create driver to settings

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from dotenv import load_dotenv
 import os
 
@@ -5,3 +7,10 @@ load_dotenv()
 MAKTABKHOONE_EMAIL = os.getenv("MAKTABKHOONE_EMAIL")
 MAKTABKHOONE_PASSWORD = os.getenv("MAKTABKHOONE_PASSWORD")
 MAKTABKHOONE_URL = "https://maktabkhooneh.org"
+
+
+def create_driver() -> WebDriver:
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=options)
+    return driver
